2ed1c9f2721e1f21061dec8f5c5a236a049e98b1
HamAamoud <160150191+HamAamoud@users.noreply.github.com>
2026-08-19 11:11:01 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+M1T1_Aamoud
+8/19/26
+First C++ Program - Smoke Test
+*/
+
+//Starts Program
+#include <iostream>
+using namespace std;
+
+//Main : Function (aka a Verb)
+int main()
+{
+    cout << "Hello CSC 134!" << endl;
+    //Creating a variable
+    string name;
+    cout << "What's your name ";
+    cin >> name; //Reads the next input and assigns it to the listed variable
+
+    cout << "This program is for : " << name << endl;
+    return 0;
+}
